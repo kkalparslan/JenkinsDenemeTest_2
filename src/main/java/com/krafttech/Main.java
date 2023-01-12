f46cdddbccdf264c0223fec15a6db410d490460b
alparslan <alparslanztk@gmail.com>
2023-01-12 14:17:18 +0300

    new code added

--- src/main/java/com/krafttech/Main.java
+++ src/main/java/com/krafttech/Main.java
@@ -1,4 +1,6 @@
-package com.krafttech;public class Main {
+package com.krafttech;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
