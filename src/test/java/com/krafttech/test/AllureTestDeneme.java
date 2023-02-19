528f5602c3c81becb0cbc48c745103a55bf8dcd1
alparslan <alparslanztk@gmail.com>
2023-02-19 15:40:13 +0300

    new allure codes added 19.02.2023

---
+++ src/test/java/com/krafttech/test/AllureTestDeneme.java
@@ -0,0 +1,42 @@
+package com.krafttech.test;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
+import org.testng.annotations.Test;
+
+public class AllureTestDeneme {
+    @Test(description = "Test senaryosu açıklaması")
+    @Description("Senaryonun genel açıklaması")
+    public void testScenario() {
+        launchApplication();
+        loginToApplication();
+        performSearch();
+        selectSearchResult();
+        logoutFromApplication();
+    }
+
+    @Step("Uygulama başlatılıyor")
+    public void launchApplication() {
+        // Uygulama başlatma işlemleri burada yapılır
+    }
+
+    @Step("Uygulamaya giriş yapılıyor")
+    public void loginToApplication() {
+        // Giriş yapma işlemleri burada yapılır
+    }
+
+    @Step("Arama yapılıyor")
+    public void performSearch() {
+        // Arama işlemleri burada yapılır
+    }
+
+    @Step("Arama sonuçlarından biri seçiliyor")
+    public void selectSearchResult() {
+        // Arama sonuçlarından seçim yapma işlemleri burada yapılır
+    }
+
+    @Step("Uygulamadan çıkış yapılıyor")
+    public void logoutFromApplication() {
+        // Çıkış yapma işlemleri burada yapılır
+    }
+}
--- src/test/java/com/krafttech/test/Jenkins_Test_1.java
+++ src/test/java/com/krafttech/test/Jenkins_Test_1.java
@@ -1,6 +1,7 @@
 package com.krafttech.test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,6 +14,57 @@ public class Jenkins_Test_1 {
 
     @Test
     public static void jenkinsDenemeTest1() throws InterruptedException {
+
+        System.out.println("test başarılı");
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+
+//        driver.get("http://google.com");
+//        WebElement searchBox = driver.findElement(By.xpath("//input[@class='gLFyf']"));
+//        //searchBox.click();
+//        searchBox.sendKeys(("mhrs")+ ENTER);
+//        JavascriptExecutor jse= (JavascriptExecutor) driver;
+//        jse.executeScript("arguments[0].scrollIntoView(true);"
+//                +"arguments[0].click()",searchBox);
+        driver.get("https://www.mhrs.gov.tr/");
+        driver.manage().window().maximize();
+        driver.findElement(By.className("headerRandevual")).click();
+        Thread.sleep(2000);
+
+        String currentTab = driver.getWindowHandle();
+        Set<String> windowHandles = driver.getWindowHandles();
+
+        for (String tab : windowHandles) {
+            if(!tab.equals(currentTab)) {
+                driver.switchTo().window(tab);
+            }
+        }
+        Thread.sleep(2000);
+        //System.out.println("driver.getCurrentUrl() = " + driver.getCurrentUrl());
+
+        WebElement tc = driver.findElement(By.xpath("//input[@id='LoginForm_username']"));
+        tc.click();
+        tc.sendKeys("15017265816");
+        Thread.sleep(2000);
+        WebElement psword=driver.findElement(By.xpath("//input[@id='LoginForm_password']"));
+        psword.click();
+        psword.sendKeys("Qw098765");
+        WebElement login=driver.findElement(By.xpath("//button[@class='ant-btn ant-btn-teal ant-btn-block']"));
+        login.click();
+        Thread.sleep(2000);
+//
+//
+        driver.findElement(By.xpath("//button[@class='ant-btn']")).click();
+//        Alert alert=driver.switchTo().alert();
+//        alert.accept();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("(//div[@class='randevu-turu-grup-article'])[3]")).click();
+        Thread.sleep(2000);
+        driver.quit();
+
+    }
+    @Step
+    public static void allureDeneme() throws InterruptedException {
 
         System.out.println("test başarılı");
         WebDriverManager.chromedriver().setup();
